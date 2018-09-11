a8cc81df5b1097b7ebffc50bd8ebfe366a84a520
xujiahuicoffee <953323966@qq.com>
2018-09-11 21:35:29 +0800

    提交整个springboot项目

---
+++ src/main/java/com/coffee/jiahui/JiahuiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.coffee.jiahui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiahuiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiahuiDemoApplication.class, args);
+	}
+}
